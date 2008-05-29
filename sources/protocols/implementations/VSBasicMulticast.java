c9c76b1ef56707834c7b26342031cb3de8f72369
Paul Buetow <paul@buetow.org>
2008-05-29 14:04:26 +0000

    initial basic multicast protocol

---
+++ sources/protocols/implementations/VSBasicMulticast.java
@@ -0,0 +1,85 @@
+/*
+ * VS is (c) 2008 by Paul C. Buetow
+ * vs@dev.buetow.org
+ */
+package protocols.implementations;
+
+import protocols.VSAbstractProtocol;
+import core.VSMessage;
+
+/**
+ * The Class VSBasicMulticastProtocol.
+ */
+public class VSBasicMulticastProtocol extends VSAbstractProtocol {
+	/** The serial version uid */
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Instantiates a new dummy protocol.
+     */
+    public VSBasicMulticastProtocol() {
+        super(VSAbstractProtocol.HAS_ON_CLIENT_START);
+        setClassname(getClass().toString());
+    }
+
+    /* (non-Javadoc)
+     * @see events.VSAbstractProtocol#onClientInit()
+     */
+    public void onClientInit() {
+    }
+
+    /* (non-Javadoc)
+     * @see protocols.VSAbstractProtocol#onClientReset()
+     */
+    public void onClientReset() {
+    }
+
+    /* (non-Javadoc)
+     * @see protocols.VSAbstractProtocol#onClientStart()
+     */
+    public void onClientStart() {
+    }
+
+    /* (non-Javadoc)
+     * @see protocols.VSAbstractProtocol#onClientRecv(core.VSMessage)
+     */
+    public void onClientRecv(VSMessage recvMessage) {
+    }
+
+    /* (non-Javadoc)
+     * @see protocols.VSAbstractProtocol#onClientSchedule()
+     */
+    public void onClientSchedule() {
+    }
+
+    /* (non-Javadoc)
+     * @see events.VSAbstractProtocol#onServerInit()
+     */
+    public void onServerInit() {
+    }
+
+    /* (non-Javadoc)
+     * @see protocols.VSAbstractProtocol#onServerReset()
+     */
+    public void onServerReset() {
+    }
+
+    /* (non-Javadoc)
+     * @see protocols.VSAbstractProtocol#onServerRecv(core.VSMessage)
+     */
+    public void onServerRecv(VSMessage recvMessage) {
+    }
+
+    /* (non-Javadoc)
+     * @see protocols.VSAbstractProtocol#onServerSchedule()
+     */
+    public void onServerSchedule() {
+    }
+
+    /* (non-Javadoc)
+     * @see protocols.VSAbstractProtocol#toString()
+     */
+    public String toString() {
+        return super.toString();
+    }
+}
