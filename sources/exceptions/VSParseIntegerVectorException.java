ffaa455d9b9c14714d3b5aa440a0a8489b5de9ee
Paul Buetow <paul@buetow.org>
2008-05-29 20:00:52 +0000

    ok

--- sources/exceptions/VSParseIntegerVectorException.java
+++ sources/exceptions/VSParseIntegerVectorException.java
@@ -1,4 +1,15 @@
+/*
+ * VS is (c) 2008 by Paul C. Buetow
+ * vs@dev.buetow.org
+ */
 package exceptions;
 
+/**
+ * The Interface VSParseIntegerVectorException. This exception is thrown if
+ * the VSAbstractEditor is not able to parse the vector fields input of the
+ * user.
+ *
+ * @author Paul C. Buetow
+ */
 public class VSParseIntegerVectorException extends Exception {
 }
